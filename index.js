35230c6c5c089960299fd7d75c9bd8b456ae51fc
Abhisit Ukham <abhisit@ukham.com>
2022-10-27 22:35:54 +0700

    update

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+var cors = require('cors')
+const fs = require('fs');
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+
+const books = require("./db")
+
+const saveBook = (data) => {
+    let results = JSON.stringify(data);
+    fs.writeFileSync("db.json", results)
+}
+
+app.get('/books', (req, res) => {
+    res.send(books)
+})
+
+app.get('/books/:id', (req, res) => {
+    res.send(books.find(x => x.id == req.params.id))
+})
+
+app.post('/books', (req, res) => {
+    books.push(req.body)
+    saveBook(books)
+    res.status(201).json(req.body)
+})
+
+app.put('/books/:id', (req, res) => {
+    const updateIndex = books.findIndex(x => x.id == req.params.id)
+    Object.assign(books[updateIndex], req.body)
+    saveBook(books)
+    res.status(200).json(req.body)
+})
+
+app.delete('/books/:id', (req, res) => {
+    const deletedIndex = books.findIndex(book => book.id === req.params.id)
+    books.splice(deletedIndex, 1)
+    saveBook(books)
+    res.status(204).send()
+ })
+
+app.listen(3000, () => {
+    console.log('Start server at port 3000.')
+})
